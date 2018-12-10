a8a64b1d051688ed153901fa8450fc9362ef8e53
l166320 <45619620+l166320@users.noreply.github.com>
2018-12-10 23:05:00 +0500

    Add files via upload

--- user.java
+++ user.java
@@ -5,11 +5,20 @@
  */
 package linked_in;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author dell
  */
 public class user {
+     Connection c=null;
+    Statement s1=null;
+    ResultSet r = null;
     /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -35,7 +44,8 @@ public class user {
         return u;
     }
     
-    public void setValues(String un, String pass, String nm, String em, String coun, String uni, String deg, String prof, String web, int sal){
+    public void setValues(int id,String un, String pass, String nm, String em, String coun, String uni, String deg, String prof, String web, int sal){
+        user_id =id;
         username = un;
         password = pass;
         name = nm;
@@ -46,6 +56,19 @@ public class user {
         profession = prof;
         website = web;
         salary = sal;
+          try 
+        {
+            c=DriverManager.getConnection("jdbc:derby://localhost:1527/linkedin","event","123");
+            s1=c.createStatement();
+            System.out.println("Hello world");
+            int y2 = s1.executeUpdate("INSERT INTO EVENT.USER_DATA VALUES("+id+",'"+un+"','"+pass+"','"+nm+"','"+em+"','"+coun+"','"+uni+"','"+deg+"','"+prof+"','"+web+"',"+sal+")");
+        } 
+        catch (SQLException ex) 
+        {
+           // Logger.getLogger(event.class.getName()).log(Level.SEVERE, null, ex);
+             System.out.println("no connection");
+        }
+        
     }
    
 }
